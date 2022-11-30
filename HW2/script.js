7567271feae69f451d37042b5b34022ad192628c
Dmitry Shumskiy <ejedima260@gmail.com>
2022-11-30 13:59:45 +0200

    HomeWork

---
+++ HW2/script.js
@@ -0,0 +1,9 @@
+const firstNum = +prompt('Enter first num', '');
+const secondNum = +prompt('Enter second num', '');
+
+const resultString = `a + b = ${firstNum + secondNum}\n
+a - b = ${firstNum - secondNum}\n
+a / b = ${firstNum - secondNum}\n
+a * b = ${firstNum * secondNum}`;
+
+alert(resultString);
